0678aae8c357dd05b135cfb9cfb817d97d8f8448
Muhammad Zunnoorain Rafi <bhoshyl@gmail.com>
2023-07-31 11:42:41 +0500

    third

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -42,7 +42,7 @@ app.use('/', blogRoute);
 
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
